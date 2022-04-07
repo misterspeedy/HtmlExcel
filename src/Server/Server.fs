638502616c5f64363453d8f35131794bed1bb409
Kit Eason <kit.eason@perpetuum.com>
2022-04-07 18:28:45 +0100

    Handle unavailable url.

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -49,89 +49,110 @@ type BoolMap() =
         and set (x, y) value =
             d.[(x, y)] <- value
 
-let forceSchema (url : string) =
-    let uriBuilder = new UriBuilder(url)
-    uriBuilder.Scheme <- "https"
-    uriBuilder.Port <- -1
-    uriBuilder.ToString()
+module Url =
+
+    open System.Net
+
+    let forceSchema (url : string) =
+        let uriBuilder = new UriBuilder(url)
+        uriBuilder.Scheme <- "https"
+        uriBuilder.Port <- -1
+        uriBuilder.ToString()
+
+    let isAvailable (url : string) =
+        try
+            let request = WebRequest.Create(url) :?> HttpWebRequest
+            request.Method <- "HEAD"
+            use response = request.GetResponse() :?> HttpWebResponse
+            response.StatusCode = HttpStatusCode.OK
+        with
+        | _ ->
+            false
 
 let doGetTables (url : string) =
     async {
         try
-            let! htmlDoc = HtmlDocument.AsyncLoad (url |> forceSchema)
-            let mutable tablesFound = false
 
-            let cells =
-                [
-                    let tables = htmlDoc.Descendants ["table"]
-                    let mutable tableIndex = 1
-                    for table in tables do
-                        // TODO render <thead>s and <tbody> separately.
-                        // Doing this will ensure that rowspans end correctly, e.g. https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_th_rowspan_0
-                        let allRows =
-                            table.Descendants ["tr"]
-                        if allRows |> Seq.length > 1 then
-                            // TODO use <caption> to name (ensuring unique)
-                            tablesFound <- true
-                            Worksheet $"Table{tableIndex}"
-                            tableIndex <- tableIndex + 1
+            let url = url |> Url.forceSchema
 
-                            let boolMap = BoolMap()
+            if url |> Url.isAvailable then
 
-                            let rowCount = allRows |> Seq.length
+                let! htmlDoc = HtmlDocument.AsyncLoad (url |> Url.forceSchema)
+                let mutable tablesFound = false
 
-                            for rowIndex, row in allRows |> Seq.indexed do
+                let cells =
+                    [
+                        let tables = htmlDoc.Descendants ["table"]
+                        let mutable tableIndex = 1
+                        for table in tables do
+                            // TODO render <thead>s and <tbody> separately.
+                            // Doing this will ensure that rowspans end correctly, e.g. https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_th_rowspan_0
+                            let allRows =
+                                table.Descendants ["tr"]
+                            if allRows |> Seq.length > 1 then
+                                // TODO use <caption> to name (ensuring unique)
+                                tablesFound <- true
+                                Worksheet $"Table{tableIndex}"
+                                tableIndex <- tableIndex + 1
 
-                                let thds = row.Descendants ["th"; "td"] |> Array.ofSeq
+                                let boolMap = BoolMap()
 
-                                // Set up a map of cells where there is no corresponding th/td
-                                // element because there is a th/td with a colSpan > 1 above.
-                                let mutable colIndex = 0
-                                for thd in thds do
+                                let rowCount = allRows |> Seq.length
 
-                                    let rowSpan =
-                                        thd
-                                        |> Attribute.getAsIntOr 1 "rowspan"
-                                        |> fun cs ->
-                                            if cs > 0 then cs
-                                            // A rowSpan of 0 means span everything down to the bottom:
-                                            else rowCount - rowIndex
+                                for rowIndex, row in allRows |> Seq.indexed do
 
-                                    for offset in 1..rowSpan-1 do
-                                        boolMap.[rowIndex+offset, colIndex] <- true
-                                    colIndex <- colIndex + (thd |> Attribute.getAsIntOr 1 "colspan")
+                                    let thds = row.Descendants ["th"; "td"] |> Array.ofSeq
 
-                                let mutable thdIndex = 0
-                                let mutable colIndex = 0
+                                    // Set up a map of cells where there is no corresponding th/td
+                                    // element because there is a th/td with a colSpan > 1 above.
+                                    let mutable colIndex = 0
+                                    for thd in thds do
 
-                                while thdIndex < thds.Length do
-                                    if boolMap.[rowIndex, colIndex] then
-                                        // This cell has a th/td with rowSpan > 1 above it:
-                                        Cell []
-                                        colIndex <- colIndex + 1
-                                    else
-                                        let thd = thds.[thdIndex]
-                                        let colSpan = thd |> Attribute.getAsIntOr 1 "colspan"
+                                        let rowSpan =
+                                            thd
+                                            |> Attribute.getAsIntOr 1 "rowspan"
+                                            |> fun cs ->
+                                                if cs > 0 then cs
+                                                // A rowSpan of 0 means span everything down to the bottom:
+                                                else rowCount - rowIndex
 
-                                        Cell [
-                                            String (thd.InnerText())
-                                            if thd.Name() = "th" then FontEmphasis Bold
-                                            Next (RightBy colSpan)
-                                        ]
-                                        thdIndex <- thdIndex + 1
-                                        colIndex <- colIndex + 1
-                                Go NewRow
+                                        for offset in 1..rowSpan-1 do
+                                            boolMap.[rowIndex+offset, colIndex] <- true
+                                        colIndex <- colIndex + (thd |> Attribute.getAsIntOr 1 "colspan")
 
-                            AutoFit All
-                ]
-            if tablesFound then
-                return Ok (
-                    {
-                        Name = htmlDoc |> Filename.fromHtmlDoc
-                        Bytes = cells |> Render.AsStreamBytes
-                    })
+                                    let mutable thdIndex = 0
+                                    let mutable colIndex = 0
+
+                                    while thdIndex < thds.Length do
+                                        if boolMap.[rowIndex, colIndex] then
+                                            // This cell has a th/td with rowSpan > 1 above it:
+                                            Cell []
+                                            colIndex <- colIndex + 1
+                                        else
+                                            let thd = thds.[thdIndex]
+                                            let colSpan = thd |> Attribute.getAsIntOr 1 "colspan"
+
+                                            Cell [
+                                                String (thd.InnerText())
+                                                if thd.Name() = "th" then FontEmphasis Bold
+                                                Next (RightBy colSpan)
+                                            ]
+                                            thdIndex <- thdIndex + 1
+                                            colIndex <- colIndex + 1
+                                    Go NewRow
+
+                                AutoFit All
+                    ]
+                if tablesFound then
+                    return Ok (
+                        {
+                            Name = htmlDoc |> Filename.fromHtmlDoc
+                            Bytes = cells |> Render.AsStreamBytes
+                        })
+                else
+                    return Error ("Sorry, that web page doesn't seem to contain any HTML tables.")
             else
-                return Error ("Sorry, that web page doesn't seem to contain any HTML tables.")
+                return Error ("Sorry, we couldn't reach that URL.")
         with
         | e ->
             return Error e.Message
