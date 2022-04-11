6cf78978feb86023a6e0099f36ed305baecb8a82
Kit Eason <kit.eason@perpetuum.com>
2022-04-11 19:22:39 +0100

    Colour/layout tweaks.

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -80,27 +80,31 @@ let messagePanel color (heading : string) (message : string)  =
 
 let containerBox (model: Model) (dispatch: Msg -> unit) =
     Bulma.box [
-        Bulma.field.div [
-            field.isGrouped
-            prop.children [
-                Bulma.control.p [
-                    control.isExpanded
-                    prop.children [
-                        Bulma.input.text [
-                            prop.value model.Url
-                            prop.placeholder "E.g. https://en.wikipedia.org/wiki/Asteroid"
-                            prop.onChange (SetUrl >> dispatch)
-                            prop.onKeyPress (fun kp -> if kp.key = "Enter" then GetTables |> dispatch)
+        prop.style [style.backgroundColor "#D5F5E3"]
+        prop.children [
+            Bulma.field.div [
+                field.isGrouped
+                prop.children [
+                    Bulma.control.p [
+                        control.isExpanded
+                        prop.children [
+                            Bulma.input.text [
+                                prop.value model.Url
+                                prop.style [ style.backgroundColor "White"]
+                                prop.placeholder "E.g. https://en.wikipedia.org/wiki/Asteroid"
+                                prop.onChange (SetUrl >> dispatch)
+                                prop.onKeyPress (fun kp -> if kp.key = "Enter" then GetTables |> dispatch)
+                            ]
                         ]
                     ]
-                ]
-                Bulma.control.p [
-                    Bulma.button.a [
-                        color.isDark
-                        if model.Status = Getting then Bulma.button.isLoading
-                        prop.disabled (Model.isValid model.Url |> not)
-                        prop.onClick (fun _ -> dispatch GetTables)
-                        prop.text "Download"
+                    Bulma.control.p [
+                        Bulma.button.a [
+                            color.isDark
+                            if model.Status = Getting then Bulma.button.isLoading
+                            prop.disabled (Model.isValid model.Url |> not)
+                            prop.onClick (fun _ -> dispatch GetTables)
+                            prop.text "Download"
+                        ]
                     ]
                 ]
             ]
@@ -110,13 +114,11 @@ let containerBox (model: Model) (dispatch: Msg -> unit) =
 let view (model: Model) (dispatch: Msg -> unit) =
     Bulma.hero [
         hero.isFullHeight
-        color.isWhite
+        prop.style [style.backgroundColor "#F8F9F9"]
         prop.children [
             Bulma.column [
-                column.is10
-                column.isOffset1
-                // Bulma.text.hasTextCentered
-                // Bulma.text.hasTextWeightBold
+                column.is8
+                column.isOffset2
                 prop.children [
                     Bulma.content [
                         Bulma.text.hasTextLeft
@@ -130,31 +132,27 @@ let view (model: Model) (dispatch: Msg -> unit) =
                                 ]
                                 prop.text "✔"
                             ]
+                        let bulletLine (text : string) =
+                            Html.p [
+                                prop.style [ style.marginBottom 0]
+                                prop.children [
+                                    bullet
+                                    Html.span [ prop.text text ]
+                                ]
+                            ]
                         prop.children [
                             Html.h1 [ prop.text "Get the tables from a web page as an Excel spreadsheet" ]
-                            Html.p [
-                                bullet
-                                Html.span [ prop.text "Enter the URL of a website that has HTML tables." ]
-                            ]
-                            Html.p [
-                                bullet
-                                Html.span [ prop.text "Click \"Download\"." ]
-                            ]
-                            Html.p [
-                                bullet
-                                Html.span [ prop.text "Check your browser downloads."]
-                            ]
-                            Html.p [
-                                bullet
-                                Html.span [ prop.text "Each table is in a tab in the spreadsheet."]
-                            ]
+                            bulletLine "Enter the URL of a website that has HTML tables."
+                            bulletLine "Click \"Download\"."
+                            bulletLine "Check your browser downloads."
+                            bulletLine "Each table is in a tab in the spreadsheet."
                         ]
                     ]
                 ]
             ]
             Bulma.column [
-                column.is10
-                column.isOffset1
+                column.is8
+                column.isOffset2
                 prop.children [
                     containerBox model dispatch
                 ]
@@ -173,7 +171,10 @@ let view (model: Model) (dispatch: Msg -> unit) =
                             messagePanel color.isSuccess "Yay!" $"Extracted {tc} {noun} - see your browser downloads."
                         | Error e ->
                             messagePanel color.isDanger "Oopsie!" e
-                        Html.img [ prop.src "/push-button-receive-table.png" ]
+                        Html.img [
+                            prop.style [ style.backgroundColor "White"]
+                            prop.src "/push-button-receive-table.png"
+                        ]
                     ]
                 ]
             ]
